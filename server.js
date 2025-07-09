65656854635e0bc8deafee68bd218dc252291b12
unknown <sinhashreya2003udp@gmail.com>
2025-07-09 20:27:12 +0530

    Fixed

--- server.js
+++ server.js
@@ -106,5 +106,5 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on:
   - http://localhost:${PORT}
-  - http://${localIp}:${PORT}`);  //only one port to use
+  - http://${localIp}:${PORT}`);  // only one port to use
 });
